11b8c1041c3e9caee5b50e626a1ad7a130276d2a
Crystian Ataide <crystian.ataide@comercialqueiroz.com.br>
2022-10-14 12:47:02 -0400

    Adicionando todos os arquivos reformulados para o repositório

---
+++ full stack challenge/index.js
@@ -0,0 +1,10 @@
+/*=================================================
+|||		JavaScript da Página da Minha Bio		|||
+||+---------------------------------------------+||
+|||	Autor: Crystian Ataide | Data: 04/10/2022	|||
+=================================================*/
+const teste = 'Olá Mundo'
+console.log(teste)
+
+const p = document.getElementById('dom')
+console.log(p)
