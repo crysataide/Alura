ec662230b97d675bcf8646c2d3e56df254147d62
Crystian Ataide <crystianataide@gmail.com>
2023-08-31 09:12:15 -0400

    Kotlin | Atualizações

--- alura_kotlin/bytebank/src/main.kt
+++ alura_kotlin/bytebank/src/main.kt
@@ -1,6 +1,117 @@
 fun main() {
     println("Bem vindo ao Bytebank")
 
+    val contaAlex = Conta()
+    contaAlex.titular = "Alex"
+    println(contaAlex.titular)
+    contaAlex.numero = 1000
+    contaAlex.saldo = 200.0
+
+    val contaFran = Conta()
+    contaFran.titular = "Fran"
+    contaFran.numero = 1001
+    contaFran.saldo = 300.0
+
+    println(contaFran.titular)
+    println(contaFran.numero)
+    println(contaFran.saldo)
+
+    println(contaAlex.titular)
+    println(contaAlex.numero)
+    println(contaAlex.saldo)
+
+    println("depositando na conta do Alex")
+    contaAlex.deposita(50.0)
+    println(contaAlex.saldo)
+
+    println("depositando na conta da Fran")
+    contaFran.deposita(70.0)
+    println(contaFran.saldo)
+
+    println("sacando a conta do Alex")
+    contaAlex.saca(250.00)
+    println(contaAlex)
+
+    println("sacando a conta do Fran")
+    contaFran.saca(100.00)
+    println(contaFran)
+
+    println("saque em excesso na conta do Alex")
+    contaAlex.saca(100.00)
+    println(contaAlex.saldo)
+
+    println("saque em excesso na conta da Fran")
+    contaFran.saca(500.00)
+    println(contaFran.saldo)
+
+    println("Transferência da conta da Fran para o ALex")
+    if (contaFran.transfere(100.00, contaAlex)) {
+        println("Transferência sucedida")
+    } else {
+        println("Falha na transferência")
+    }
+}
+
+class Conta {
+    var titular = "";
+    var numero = 0
+    var saldo = 0.0
+
+    fun deposita(valor: Double) {
+        this.saldo += valor
+    }
+
+    fun saca(valor: Double) {
+        if (saldo >= valor) {
+            saldo -= valor
+        }
+    }
+
+    fun transfere(valor: Double, destino: Conta): Boolean {
+        if (saldo >= valor) {
+            saldo -= valor
+            destino.saldo += valor
+            return true
+        }
+        return false
+    }
+}
+
+fun testaCopiasEReferencias() {
+    val numeroX = 10
+    var numeroY = numeroX
+    numeroY++
+
+    println("numeroX $numeroX")
+    println("numeroY $numeroY")
+
+    val contaJoao = Conta()
+    contaJoao.titular = "João"
+    var contaMaria = Conta()
+    contaMaria.titular = "Maria"
+
+    println("titular conta joao: ${contaJoao.titular}")
+    println("titular conta maria: ${contaMaria.titular}")
+
+    println(contaJoao)
+    println(contaMaria)
+}
+
+fun testaLacos() {
+    var i = 0
+
+    while (i < 5) {
+        val titular: String = "Alex $i"
+        val numeroConta: Int = 1000 + 1
+        var saldo = i + 10.0
+
+        println("Titular $titular")
+        println("Número da conta $numeroConta")
+        println("Saldo da conta $saldo")
+        println()
+        i++
+    }
+
     for (i in 5 downTo 1) {
 
         val titular: String = "Alex $i"
@@ -10,10 +121,8 @@ fun main() {
         println("Titular $titular")
         println("Número da conta $numeroConta")
         println("Saldo da conta $saldo")
-
         println()
     }
-//    testaCondicoes(saldo)
 }
 
 fun testaCondicoes(saldo: Double) {
