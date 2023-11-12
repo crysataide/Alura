d78585657d0ad09cf5617c68b3ee3ce9bffd29ef
crysataide <crystianataide@gmail.com>
2023-11-12 14:03:05 -0400

    C | Course update

--- alura_c/C-Recursos Avançados da Linguagem/mapa.h
+++ alura_c/C-Recursos Avançados da Linguagem/mapa.h
@@ -1,4 +1,5 @@
 #define EMPTY '.'
+#define GHOST 'F'
 #define PERSON '@'
 
 struct mapa {
@@ -21,3 +22,7 @@ void encontraMapa(MAPA* m, POSICAO* person, char c);
 void alocaMapa(MAPA* m);
 void imprimeMapa(MAPA* m);
 void liberaMapa(MAPA* m);
+int ehValida(MAPA* m, int x, int y);
+int ehVazia(MAPA* m, int x, int y);
+void moveMapa(MAPA* m, int xorigem, int yorigem, int xdestino, int ydestino);
+void copiaMapa(MAPA* destino, MAPA * origem);
