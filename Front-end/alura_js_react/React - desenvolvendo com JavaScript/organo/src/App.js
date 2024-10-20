d1b3385b1dac78a5f5051fa3c3ee233f44263338
crysataide <crystianataide@gmail.com>
2024-10-19 21:39:16 -0400

    finalização do formulário

--- Front-end/alura_js_react/React - desenvolvendo com JavaScript/organo/src/App.js
+++ Front-end/alura_js_react/React - desenvolvendo com JavaScript/organo/src/App.js
@@ -1,9 +1,19 @@
+import { useState } from 'react';
 import Banner from './componentes/banner/banner';
+import Formulario from './componentes/Formulario';
 
 function App() {
+  const [colaborador, setColaboradores] = useState([])
+
+  const aoNovoColaboradorAdicionado = (colaborador) => {
+    console.log(colaborador)
+    setColaboradores([...setColaboradores, colaborador])
+  }
+
   return (
     <div className="App">
       <Banner/>
+      <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
     </div>
   );
 }
